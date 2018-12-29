ef0c34a7139d81b69efa2406677ea7b19817ab3d
Faruk Karadeniz <krdnzomer@gmail.com>
2018-12-29 18:38:44 +0300

    Detail view is fixed

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -5,20 +5,38 @@ import '../resources/styles/Detail.css';
 export default class Detail extends React.Component {
   render() {
     return(
-      <div id={this.props.id} className="overlay">
-        <div className="popup">
-          <a className="close" href="#">&times;</a>
-          <div className="image-content">
+      <div className="popup" id={this.props.id}>
+        <a href="#!" className="popup-overlay"></a>
+        <div className="popup-dialog">
+          <div className="popup-content">
+            <div className="image-content">
               <img className="image" src={this.props.rawImage} alt="Hata" />
+            </div>
+            <div className="user-and-download">
               <div className="user-info">
-                <img className="profile-photo" src={this.props.user.image} />
-                <p className="user-name">{this.props.user.name}</p>
-                <a href={`https://unsplash.com/@${this.props.user.username}`} target="_blank"><p className="user-username">@{this.props.user.username}</p></a>
+                <div className="user-image-wrapper">
+                  <a href={`https://unsplash.com/@${this.props.user.username}`} target="_blank">
+                    <img className="user-image" src={this.props.user.image} alt="Hata User" />
+                  </a>
+                </div>
+                <div className="user-name-wrapper">
+                  <p className="user-name">{this.props.user.name}</p>
+                  <a href={`https://unsplash.com/@${this.props.user.username}`} target="_blank">
+                    <p className="user-username">@{this.props.user.username}</p>
+                  </a>
+                </div>
               </div>
-              <a href={this.props.rawImage} className="download-button" download>Download</a>
+              <div className="download-button">
+                <a href={this.props.rawImage} download>Download</a>
+              </div>
+            </div>
+            
           </div>
         </div>
       </div>
     );
   }
+
+
+
 }
--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -10,13 +10,12 @@ export default class ImageView extends React.Component {
   //burada iskeleti güncelemme yap
   getImage = () => {
     let newHeight = (300 * this.props.view.height) / this.props.view.width;
-    let style = {'height' : `${newHeight}px`, 'width': '300px'};
+    let style = {'height' : `${newHeight}px`};
     return(
-      <div className="box">
+      <React.Fragment>
         <a href={`#${this.props.view.id}`}><img src={this.props.view.url} style={style} key={this.props.view.url} alt="Yükleniyor"/></a>
         <Detail id={this.props.view.id} rawImage={this.props.view.download} user={this.props.user} />
-      </div>
-      
+      </React.Fragment>      
     );
 
   }
